a4d9dad68b10c9a117762a8504d877825d75fa8d
DrewAPicture <info@drewapicture.com>
2018-05-12 22:54:06 -0600

    Flesh out methods for the User_Database class, now implementing the User_Database interface.

--- includes/core/class-user-database.php
+++ includes/core/class-user-database.php
@@ -1,84 +1,63 @@
 <?php
 namespace Ensemble\Core;
 
+use Ensemble\Core\Interfaces;
+
 /**
  * Core database abstraction layer.
  *
  * @since 1.0.0
  * @abstract
  */
-abstract class Database implements Database_Interface {
+abstract class User_Database implements Interfaces\User_Database {
 
 	/**
 	 * Retrieves a single core object.
 	 *
-	 * @access public
-	 * @since  1.0.0
+	 * @since 1.0.0
 	 *
-	 * @param int $object_id Object ID.
-	 * @return \Ensemble\Model|\WP_Error Core object or WP_Error if there was a problem.
+	 * @param int $user_id User ID.
+	 * @return \WP_User|\WP_Error User object or WP_Error if there was a problem.
 	 */
-	public function get( $object_id ) {
-
+	public function get( $user_id ) {
+		return get_user_by( 'id', $user_id );
 	}
 
 	/**
-	 * Queries for core objects.
+	 * Queries for users.
 	 *
-	 * @access public
-	 * @since  1.0.0
+	 * @since 1.0.0
 	 *
 	 * @param array $query_args Query arguments.
 	 * @param bool  $count      Optional. Whether this is a count query. Default false.
-	 * @return array|int Array of results, or int if `$count` is true.
+	 * @return array|int Array of user results, or int if `$count` is true.
 	 */
-	abstract public function query( $query_args, $count = false );
+	public function query( $query_args, $count = false ) {
+		if ( true === $count ) {
+			$query_args['number'] = -1;
+			$query_args['fields'] = 'ids';
+		}
+
+		$query   = new \WP_User_Query( $query_args );
+		$results = $query->get_results();
+
+		if ( true === $count ) {
+			return count( $results );
+		} else {
+			return $results;
+		}
+	}
 
 	/**
-	 * Retrieves a count of core objects based on the given query arguments.
+	 * Retrieves a count of users based on the given query arguments.
 	 *
-	 * @access public
-	 * @since  1.0.0
+	 * @since 1.0.0
 	 *
 	 * @param array $query_args Query arguments.
 	 * @return int Number of results for the given query arguments.
 	 */
 	public function query_count( $query_args ) {
 		return $this->query( $query_args, true );
-	}
-
-	/**
-	 * Retrieves a core object instance based on the given type.
-	 *
-	 * @access protected
-	 * @since  1.0.0
-	 *
-	 * @param object|int $instance Instance or object ID.
-	 * @param string     $class    Object class name.
-	 * @return object|\WP_Error Object instance, otherwise WP_Error object if there was a problem.
-	 */
-	protected function get_core_object( $instance, $object_class ) {
-		if ( ! class_exists( $object_class ) ) {
-			return new \WP_Error( 'get_core_object_class' );
-		}
-
-		if ( $instance instanceof $object_class ) {
-			$_object = $instance;
-		} elseif ( is_object( $instance ) ) {
-			if ( isset( $instance->ID ) ) {
-				$_object = new $object_class( $instance );
-			} else {
-				$_object = $object_class::get_instance( $instance );
-			}
-		} else {
-			$_object = $object_class::get_instance( $instance );
-		}
-
-		if ( is_wp_error( $_object ) ) {
-			return $_object;
-		}
-
-		return $_object;
 	}
 
 }
