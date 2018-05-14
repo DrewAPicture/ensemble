64820af8a7ba9e8ef2f2536b0de9bfba398880d5
DrewAPicture <info@drewapicture.com>
2018-05-14 03:13:26 -0600

    Stub out a Util\Settings class.

---
+++ includes/util/class-settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Settings utility class
+ *
+ * @package   Ensemble\Util
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Util;
+
+/**
+ * Core class to facilitate global settings access.
+ *
+ * @since 1.0.0
+ */
+class Settings {
+
+}
