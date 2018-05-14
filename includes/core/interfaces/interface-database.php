b91623757a3fcd1e0732b20a01ca4bbab8816ccc
DrewAPicture <info@drewapicture.com>
2018-05-13 20:46:54 -0600

    Fully define and document get, get_object, query, and count methods for the Core\Interfaces\Database interface.

--- includes/core/interfaces/interface-database.php
+++ includes/core/interfaces/interface-database.php
@@ -24,7 +24,17 @@ interface Database {
 	 * @param int $object_id Object ID.
 	 * @return \Ensemble\Model|\WP_Error Core object or WP_Error if there was a problem.
 	 */
-	function get( $object_id );
+	public function get( $object_id );
+
+	/**
+	 * Retrieves a single core object with caching.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int|Ensemble\Core\Model $object Object or object ID.
+	 * @return Ensemble\Core\Model|\WP_Error Core object or WP_Error object.
+	 */
+	public function get_object( $object );
 
 	/**
 	 * Queries for component objects.
@@ -35,7 +45,7 @@ interface Database {
 	 * @param bool  $count      Optional. Whether this is a count query. Default false.
 	 * @return array|int Array of results, or int if `$count` is true.
 	 */
-	function query( $query_args, $count = false );
+	public function query( $query_args, $count = false );
 
 	/**
 	 * Retrieves a count of core objects based on the given query arguments.
@@ -45,6 +55,6 @@ interface Database {
 	 * @param array $query_args Query arguments.
 	 * @return int Number of results for the given query arguments.
 	 */
-	function query_count( $query_args );
+	public function count( $query_args );
 
 }
