f14d83d11c9dbf1da814f367758ff359db362897
DrewAPicture <info@drewapicture.com>
2018-05-15 11:03:38 -0600

    Implement the View_Loader interface for Venues\Admin\Delete.

--- includes/components/venues/admin/class-delete.php
+++ includes/components/venues/admin/class-delete.php
@@ -9,14 +9,16 @@
  */
 namespace Ensemble\Components\Venues\Admin;
 
-use Ensemble\Core\Interfaces\Loader;
+use Ensemble\Core\Interfaces\View_Loader;
 
 /**
  * Handles deleting venues.
  *
  * @since 1.0.0
+ *
+ * @see View_Loader
  */
-class Delete implements Loader {
+class Delete implements View_Loader {
 
 	/**
 	 * Registers hook callbacks for deleting venues.
@@ -25,6 +27,21 @@ class Delete implements Loader {
 	 */
 	public function load() {
 
+	}
+
+	/**
+	 * Loads a view based on the 'ensbl-view' $_REQUEST arg.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $args Optional. Passed-thru display arguments (if any). Default empty array.
+	 */
+	public function load_view( $args = array() ) {
+		?>
+		<div class="wrap">
+			<h1><?php esc_html_e( 'Delete Venue', 'ensemble' ); ?></h1>
+		</div>
+		<?php
 	}
 
 }
