c4fd287caaae031ac60cc4b8513dbbc6e8a4e39c
DrewAPicture <info@drewapicture.com>
2018-05-15 11:04:53 -0600

    Implement the Menu_Router interface for Contests\Admin\Menu. Handle routing view requests for contest add, edit, and delete.

--- includes/components/contests/admin/class-menu.php
+++ includes/components/contests/admin/class-menu.php
@@ -9,16 +9,17 @@
  */
 namespace Ensemble\Components\Contests\Admin;
 
-use Ensemble\Core\Interfaces\Loader;
+use Ensemble\Core\Interfaces\Menu_Router;
+use function Ensemble\load_view;
 
 /**
  * Sets up the Contests menu.
  *
  * @since 1.0.0
  *
- * @see Loader
+ * @see Menu_Router
  */
-class Menu implements Loader {
+class Menu implements Menu_Router {
 
 	/**
 	 * Initializes menu registrations.
@@ -41,21 +42,37 @@ class Menu implements Loader {
 			__( 'Contests', 'ensemble' ),
 			'manage_options',
 			'ensemble-admin-contests',
-			array( $this, 'contests_overview' )
+			array( $this, 'route_request' )
 		);
 	}
 
 	/**
-	 * Renders the secondary Contests Overview admin screen.
+	 * Routes the request based on the current ensbl-view value.
 	 *
 	 * @since 1.0.0
 	 */
-	public function contests_overview() {
-		?>
-		<div class="wrap">
-			<h1><?php esc_html_e( 'Contests Overview', 'ensemble' ); ?></h1>
-		</div>
-		<?php
+	public function route_request() {
+
+		$display_args = array(
+			'view' => isset( $_REQUEST['ensbl-view'] ) ? sanitize_key( $_REQUEST['ensbl-view' ] ) : '',
+			'id'   => isset( $_REQUEST['contest_id'] ) ? absint( $_REQUEST['contest_id'] )        :  0,
+		);
+
+		switch ( $display_args['view'] ) {
+
+			case 'add':
+			case 'edit':
+				load_view( new Save, $display_args );
+				break;
+
+			case 'delete':
+				load_view( new Delete, $display_args );
+				break;
+
+			default:
+				load_view( new Overview, $display_args );
+				break;
+		}
 	}
 
 }
