c1b04f198773804f226cd8f97519c5df4aad86a3
DrewAPicture <info@drewapicture.com>
2018-05-15 11:02:28 -0600

    Implement the View_Loader interface for Contests\Admin\Save, add a secondary switch for handling Edit vs Add.

--- includes/components/contests/admin/class-save.php
+++ includes/components/contests/admin/class-save.php
@@ -9,14 +9,14 @@
  */
 namespace Ensemble\Components\Contests\Admin;
 
-use Ensemble\Core\Interfaces\Loader;
+use Ensemble\Core\Interfaces\View_Loader;
 
 /**
  * Handles adding and editing contests in the admin.
  *
  * @since 1.0.0
  */
-class Save implements Loader {
+class Save implements View_Loader {
 
 	/**
 	 * Registers hook callbacks for adding and editing contests.
@@ -25,6 +25,59 @@ class Save implements Loader {
 	 */
 	public function load() {
 
+	}
+
+	/**
+	 * Loads a view based on the 'ensbl-view' $_REQUEST arg.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $args Optional. Passed-thru display arguments (if any). Default empty array.
+	 */
+	public function load_view( $args = array() ) {
+		if ( ! empty( $args['view'] ) && ! empty( $args['id'] ) ) {
+
+			switch( $args['view'] ) {
+				case 'edit':
+					$this->display_edit_contest( $args['id'] );
+					break;
+
+				case 'add':
+					$this->display_add_contest( $args['id'] );
+					break;
+			}
+
+		}
+	}
+
+	/**
+	 * Displays the Edit Contest screen markup.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $contest Contest ID.
+	 */
+	public function display_edit_contest( $contest ) {
+		?>
+		<div class="wrap">
+			<h1><?php esc_html_e( 'Edit Contest', 'ensemble' ); ?></h1>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Displays the Add Contest screen markup.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $contest Contest ID.
+	 */
+	public function display_add_contest( $contest ) {
+		?>
+		<div class="wrap">
+			<h1><?php esc_html_e( 'Add Contest', 'ensemble' ); ?></h1>
+		</div>
+		<?php
 	}
 
 }
