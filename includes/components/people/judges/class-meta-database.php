e44b08c0a7e3984abc4222d5525fc2153e294f48
DrewAPicture <info@drewapicture.com>
2018-05-15 14:51:08 -0600

    Introduce a Judges people component and stub out a judge-functions.php file.

---
+++ includes/components/people/judges/class-meta-database.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Sets up the meta queries for the Judges component
+ *
+ * @package   Ensemble\Components\People
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Components\People\Judges;
+
+use Ensemble\Core\User_Meta;
+
+/**
+ * Judge meta abstraction layer.
+ *
+ * @since 1.0.0
+ *
+ * @see \Ensemble\Core\User_Meta
+ */
+class Meta_Database extends User_Meta {
+
+
+}
---
+++ includes/functions/judge-functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Director Functions
+ *
+ * @package   Ensemble\Functions
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble;
