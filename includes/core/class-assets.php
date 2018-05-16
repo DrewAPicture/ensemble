9787b0a6f8d9d51f2a385151dabb793887ab6e15
DrewAPicture <info@drewapicture.com>
2018-05-15 20:26:18 -0600

    Rename some script and style handles, change the version for bootstrap assets to 4.0.0, register a new (empty) ensbl-admin script and cite parsley (and therefore bootstrap) as dependencies.

--- includes/core/class-assets.php
+++ includes/core/class-assets.php
@@ -48,15 +48,18 @@ class Assets implements Interfaces\Loader {
 		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.min' : '';
 
 		// Stylesheets.
-		wp_register_style( 'esbl-bootstrap', ENSEMBLE_PLUGIN_URL . '/assets/css/bootstrap' . $suffix . '.css', array(), '3.0.0' );
-		wp_register_style( 'esbl-admin', ENSEMBLE_PLUGIN_URL . '/assets/css/ensemble-admin.css', array( 'esbl-bootstrap' ), ENSEMBLE_VERSION );
-
-		if ( false !== strpos( $hook_suffix, 'page_ensemble-admin' ) ) {
-			wp_enqueue_style( 'esbl-admin' );
-		}
+		wp_register_style( 'ensbl-bootstrap-css', ENSEMBLE_PLUGIN_URL . '/assets/css/bootstrap' . $suffix . '.css', array(), '4.0.0' );
+		wp_register_style( 'ensbl-admin-css', ENSEMBLE_PLUGIN_URL . '/assets/css/ensemble-admin.css', array( 'ensbl-bootstrap-css' ), ENSEMBLE_VERSION );
 
 		// Scripts.
-		wp_register_script( 'esbl-parsley', ENSEMBLE_PLUGIN_URL . '/assets/js/parsley.js', array( 'jquery' ), '2.8.1' );
+		wp_register_script( 'ensbl-bootstrap', ENSEMBLE_PLUGIN_URL . '/assets/js/bootstrap' . $suffix . '.js', array( 'jquery' ), '4.0.0' );
+		wp_register_script( 'ensbl-parsley', ENSEMBLE_PLUGIN_URL . '/assets/js/parsley.js', array( 'jquery', 'ensbl-bootstrap' ), '2.8.1' );
+		wp_register_script( 'ensbl-admin', ENSEMBLE_PLUGIN_URL . '/assets/js/ensemble-admin.js', array( 'ensbl-parsley' ), ENSEMBLE_VERSION );
+
+		if ( false !== strpos( $hook_suffix, 'page_ensemble-admin' ) ) {
+			wp_enqueue_style( 'ensbl-admin-css' );
+			wp_enqueue_script( 'ensbl-admin' );
+		}
 	}
 
 }
