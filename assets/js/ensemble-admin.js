9a7aa01c152eb6ee143b58ba6cb2d2bfd772ffb6
DrewAPicture <info@drewapicture.com>
2018-05-16 13:47:36 -0600

    Add logic to ensemble-admin.js to automatically turn selects, multi-selects, and date fields into selectWoo and datepicker fields, respectively.

--- assets/js/ensemble-admin.js
+++ assets/js/ensemble-admin.js
@@ -1,2 +1,28 @@
 /** global ensbl_vars */
+jQuery(document).ready(function($) {
 
+	// Multi-select selectWoo instances.
+	$('select[multiple="multiple"]').each(function () {
+		$(this).selectWoo({
+			width: '100%',
+			// theme: 'bootstrap'
+		});
+	});
+
+	// selectWoo instances for selects.
+	$('select').each(function () {
+		$(this).selectWoo({
+			width: '100%',
+			theme: 'bootstrap'
+		});
+	});
+
+	// Datepickers.
+	$('.date').each(function () {
+		console.log($(this));
+		$(this).datepicker({
+			dateFormat: 'mm/dd/yy'
+		});
+	});
+
+} );
