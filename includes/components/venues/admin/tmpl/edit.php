03c190af2904476414cf4deb3623a1ecb3587dd7
DrewAPicture <info@drewapicture.com>
2018-05-15 20:28:47 -0600

    Add actual template files for loading venue admin views.

---
+++ includes/components/venues/admin/tmpl/edit.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Edit Venue Template
+ *
+ * @package   Ensemble\Admin\Templates
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Components\Venues\Admin;
+?>
+<div class="wrap">
+	<h1 class="wp-heading-inline"><?php esc_html_e( 'Edit Venue', 'ensemble' ); ?></h1>
+</div>
