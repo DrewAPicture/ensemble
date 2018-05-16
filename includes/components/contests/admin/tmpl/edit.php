de5810bb04d30caf3d3caf660e00ff887aca792f
DrewAPicture <info@drewapicture.com>
2018-05-15 20:28:30 -0600

    Add actual template files for loading contest admin views.

---
+++ includes/components/contests/admin/tmpl/edit.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Edit Contest Template
+ *
+ * @package   Ensemble\Admin\Templates
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Components\Contests\Admin;
+?>
+<div class="wrap">
+	<h1 class="wp-heading-inline"><?php esc_html_e( 'Edit Contest', 'ensemble' ); ?></h1>
+</div>
---
+++ includes/components/contests/admin/tmpl/overview.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Contest Overview Template
+ *
+ * @package   Ensemble\Admin\Templates
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Components\Contests\Admin;
+?>
+<div class="wrap">
+	<h1 class="wp-heading-inline"><?php esc_html_e( 'Contests Overview', 'ensemble' ); ?></h1>
+	<a href="<?php echo esc_url( add_query_arg( array( 'ensbl-view' => 'add' ) ) ); ?>" class="page-title-action" role="button">
+		<?php esc_html_e( 'Add New', 'ensemble' ); ?>
+	</a>
+	<?php
+	$list_table = new List_Table();
+	$list_table->prepare_items();
+	?>
+	<form id="ensemble-contests" method="get">
+		<?php
+		$list_table->search_box( __( 'Search', 'ensemble' ), 'ensemble-contests-search' );
+		$list_table->views();
+		$list_table->display();
+		?>
+	</form>
+</div>
