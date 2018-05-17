d9680b1727b6c7525d6fd244349f24b86be61304
DrewAPicture <info@drewapicture.com>
2018-05-17 16:28:34 -0600

    Stub out a Template_Loader class and load it from the bootstrap.

---
+++ includes/core/class-template-loader.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Front-end Template :oader
+ *
+ * @package   Ensemble\Core
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Core;
+
+use Ensemble\Core\Interfaces\Loader;
+
+/**
+ * Core class to load template parts on the front-end.
+ *
+ * @since 1.0.0
+ *
+ * @see Loader
+ */
+class Template_Loader implements Loader {
+
+	/**
+	 * Registers callbacks for template loading purposes.
+	 *
+	 * @since 1.0.0
+	 */
+	public function load() {
+
+	}
+
+}
