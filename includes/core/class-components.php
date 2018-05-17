9b9c922ffc5855990bae71fe5b9ab35a06c24b10
DrewAPicture <info@drewapicture.com>
2018-05-17 03:44:42 -0600

    Register the Units taxonomy via Ensemble\Components.

--- includes/core/class-components.php
+++ includes/core/class-components.php
@@ -32,6 +32,50 @@ class Components implements Interfaces\Loader {
 		load( new Component\People\Staff\Setup );
 
 		load( new Component\Venues\Setup );
+
+		add_action( 'init', array( $this, 'register_taxonomies' ) );
 	}
 
+	/**
+	 * Registers taxonomies used in Ensemble core.
+	 *
+	 * @since 1.0.0
+	 */
+	public function register_taxonomies() {
+		// Competing Units taxonomy.
+		register_taxonomy( 'ensemble_unit', array(), array(
+			'hierarchical'      => false,
+			'public'            => true,
+			'show_in_nav_menus' => true,
+			'show_ui'           => true,
+			'show_admin_column' => false,
+			'query_var'         => true,
+			'rewrite'           => true,
+			'show_in_rest'      => false,
+			'labels'            => array(
+				'name'                       => __( 'Units', 'ensemble' ),
+				'singular_name'              => _x( 'Unit', 'taxonomy general name', 'ensemble' ),
+				'search_items'               => __( 'Search Units', 'ensemble' ),
+				'popular_items'              => __( 'Popular Units', 'ensemble' ),
+				'all_items'                  => __( 'All Units', 'ensemble' ),
+				'parent_item'                => __( 'Parent Unit', 'ensemble' ),
+				'parent_item_colon'          => __( 'Parent Unit:', 'ensemble' ),
+				'edit_item'                  => __( 'Edit Unit', 'ensemble' ),
+				'update_item'                => __( 'Update Unit', 'ensemble' ),
+				'view_item'                  => __( 'View Unit', 'ensemble' ),
+				'add_new_item'               => __( 'New Unit', 'ensemble' ),
+				'new_item_name'              => __( 'New Unit', 'ensemble' ),
+				'separate_items_with_commas' => __( 'Separate Units with commas', 'ensemble' ),
+				'add_or_remove_items'        => __( 'Add or remove Units', 'ensemble' ),
+				'choose_from_most_used'      => __( 'Choose from the most used Units', 'ensemble' ),
+				'not_found'                  => __( 'No Units found.', 'ensemble' ),
+				'no_terms'                   => __( 'No Units', 'ensemble' ),
+				'menu_name'                  => __( 'Units', 'ensemble' ),
+				'items_list_navigation'      => __( 'Units list navigation', 'ensemble' ),
+				'items_list'                 => __( 'Units list', 'ensemble' ),
+				'most_used'                  => _x( 'Most Used', 'ensemble_unit', 'ensemble' ),
+				'back_to_items'              => __( '&larr; Back to Units', 'ensemble' ),
+			),
+		) );
+	}
 }
