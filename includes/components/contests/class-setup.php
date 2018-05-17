47a92d0ffde308756c18f4e41362f2944bc206f5
DrewAPicture <info@drewapicture.com>
2018-05-17 01:34:02 -0600

    Add map_meta_cap filters for contests and venues.

--- includes/components/contests/class-setup.php
+++ includes/components/contests/class-setup.php
@@ -27,12 +27,42 @@ class Setup implements Loader {
 	 * @since 1.0.0
 	 */
 	public function load() {
+		add_filter( 'map_meta_cap', array( $this, 'map_meta_caps' ), 10, 4 );
+
 		require_once __DIR__ . '/functions.php';
 
 		if ( is_admin() ) {
 			load( new Admin\Menu );
 			load( new Admin\Actions );
 		}
+	}
+
+	/**
+	 * Maps meta capabilities to primitive ones.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array  $caps    The user's actual capabilities.
+	 * @param string $cap     Capability name.
+	 * @param int    $user_id The user ID.
+	 * @param array  $args    Adds the context to the cap. Typically the object ID.
+	 * @return array (Maybe) modified capabilities.
+	 */
+	public function map_meta_caps( $caps, $cap, $user_id, $args ) {
+		switch( $cap ) {
+			case 'add_contest':
+				$caps[] = 'manage_contests';
+				break;
+
+			case 'edit_contest':
+			case 'delete_contest':
+				$contest = get_contest( $args[0] );
+
+				$caps[] = is_wp_error( $contest ) ? 'do_not_allow' : 'manage_contests';
+				break;
+		}
+
+		return $caps;
 	}
 
 }
--- includes/components/venues/class-setup.php
+++ includes/components/venues/class-setup.php
@@ -27,6 +27,8 @@ class Setup implements Loader {
 	 * @since 1.0.0
 	 */
 	public function load() {
+		add_filter( 'map_meta_cap', array( $this, 'map_meta_caps' ), 10, 4 );
+
 		require_once __DIR__ . '/functions.php';
 
 		if ( is_admin() ) {
@@ -34,5 +36,34 @@ class Setup implements Loader {
 			load( new Admin\Actions );
 		}
 	}
+
+	/**
+	 * Maps meta capabilities to primitive ones.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array  $caps    The user's actual capabilities.
+	 * @param string $cap     Capability name.
+	 * @param int    $user_id The user ID.
+	 * @param array  $args    Adds the context to the cap. Typically the object ID.
+	 * @return array (Maybe) modified capabilities.
+	 */
+	public function map_meta_caps( $caps, $cap, $user_id, $args ) {
+		switch( $cap ) {
+			case 'add_venue':
+				$caps[] = 'manage_venues';
+				break;
+
+			case 'edit_venue':
+			case 'delete_venue':
+				$venue = get_venue( $args[0] );
+
+				$caps[] = is_wp_error( $venue ) ? 'do_not_allow' : 'manage_venues';
+				break;
+		}
+
+		return $caps;
+	}
+
 
 }
