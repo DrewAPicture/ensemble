2f871be40d97e02bbb1c86ec8ea838e5d7a49c8f
DrewAPicture <info@drewapicture.com>
2018-05-17 03:26:47 -0600

    JS spacing.

--- assets/js/ensemble-admin.js
+++ assets/js/ensemble-admin.js
@@ -1,32 +1,59 @@
 /** global ensbl_vars */
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function( $ ) {
 
 	// Multi-select selectWoo instances.
-	$('select[multiple="multiple"]').each(function () {
-		$(this).selectWoo({
+	$( 'select[ multiple="multiple"]' ).each( function () {
+		$( this ).selectWoo( {
 			width: '100%',
 			theme: 'bootstrap'
-		});
-	});
+		} );
+	} );
 
 	// selectWoo instances for selects.
-	$('select').each(function () {
+	$( 'select' ).each( function () {
 		// Skip it for list table bulk actions selects.
 		if ( ! $( this ).parent().hasClass( 'bulkactions' ) ) {
-			$(this).selectWoo({
+			$( this ).selectWoo({
 				width: '100%',
 				theme: 'bootstrap'
-			});
+			} );
 		}
-	});
+	} );
 
 	// Datepickers.
-	$('.date').each(function () {
-		console.log($(this));
-		$(this).datepicker({
+	$( '.date' ).each( function () {
+		$( this ).datepicker( {
 			dateFormat: 'mm/dd/yy',
 			minDate: 0
-		});
-	});
+		} );
+	} );
+
+	$( '.next' ).on( 'click', function( event ) {
+
+		var nextId = $( this ).parents( '.tab-pane' ).next().attr( 'id' );
+
+		$('[href=#' + nextId + ']' ).tab( 'show' );
+
+		return false;
+	} );
+
+	$( 'a[data-toggle="tab"]' ).on( 'shown.bs.tab', function ( event ) {
+
+		var step = $( event.target ).data( 'step' );
+		var percent = ( parseInt( step ) / 5 ) * 100;
+
+		$( '.progress-bar' ).css( {
+			width: percent + '%'
+		} );
+
+		$( '.progress-bar' ).text( 'Step ' + step + ' of 5' );
+
+	} );
+
+	$( '.first' ).on( 'click', function( event ) {
+
+		$( '#myWizard a:first' ).tab( 'show' );
+
+	} );
 
 } );
