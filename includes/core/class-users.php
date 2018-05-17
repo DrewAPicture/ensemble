f66796fd8e1c02ae8da5255262229bb5d1750153
DrewAPicture <info@drewapicture.com>
2018-05-17 00:36:11 -0600

    Stub out a Core\Users class.

---
+++ includes/core/class-users.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Sets up Users functionality
+ *
+ * @package   Ensemble\Core
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Core;
+use Ensemble\Core\Interfaces\Loader;
+
+/**
+ * Sets up the ability to manipulate various components as users.
+ *
+ * @since 1.0.0
+ */
+class Users implements Loader {
+
+	/**
+	 * Registers any hook callbacks needed to integration with the WordPress Users API.
+	 *
+	 * @since 1.0.0
+	 */
+	public function load() {
+
+	}
+
+}
