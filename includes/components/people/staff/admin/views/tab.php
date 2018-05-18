469092ccc73f0cc5c570893b017139e535deba7b
DrewAPicture <info@drewapicture.com>
2018-05-18 12:52:11 -0600

    Add admin components for People\Staff.

---
+++ includes/components/people/staff/admin/views/tab.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Circuit Staff Tab View
+ *
+ * @package   Ensemble\Admin\Templates
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Core;
+
+use function Ensemble\{load_view};
+?>
+<div class="bootstrap-iso">
+	Staff: Something goes here.
+</div>
