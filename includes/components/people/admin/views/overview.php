d6d230ef9de1abfaf6b09c7284e88b7575dea0fa
DrewAPicture <info@drewapicture.com>
2018-05-17 23:25:03 -0600

    Treat People as a full-fledged component by registering a submenu item, view loader, Actions class, and an overview view.

---
+++ includes/components/people/admin/views/overview.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * People Overview Template
+ *
+ * @package   Ensemble\Admin\Templates
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Core;
+
+use function Ensemble\{load_view};
+?>
+<h1 class="wp-heading-inline"><?php esc_html_e( 'People', 'ensemble' ); ?></h1>
---
+++ includes/components/people/class-setup.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Sets up the People component
+ *
+ * @package   Ensemble\Components\People
+ * @copyright Copyright (c) 2018, Drew Jaynes
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since     1.0.0
+ */
+namespace Ensemble\Components\People;
+
+use Ensemble\Core\Interfaces\Loader;
+use function Ensemble\{load};
+
+/**
+ * Implements People component functionality in Ensemble core.
+ *
+ * @since 1.0.0
+ *
+ * @see Ensemble\Core\Interfaces\Loader
+ */
+class Setup implements Loader {
+
+	/**
+	 * Initializes the component.
+	 *
+	 * @since 1.0.0
+	 */
+	public function load() {
+		if ( is_admin() ) {
+			load( new Admin\Menu );
+			load( new Admin\Actions );
+		}
+	}
+
+}
