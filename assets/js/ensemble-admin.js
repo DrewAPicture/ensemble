471053b2ecc494d8f0119dfd96c911d81c8cf5a3
DrewAPicture <info@drewapicture.com>
2018-05-19 02:30:37 -0600

    Trim out some unused code from ensemble-admin.js.

--- assets/js/ensemble-admin.js
+++ assets/js/ensemble-admin.js
@@ -33,32 +33,4 @@ jQuery( document ).ready( function( $ ) {
 		$( this ).datepicker( $args );
 	} );
 
-	$( '.next' ).on( 'click', function( event ) {
-
-		var nextId = $( this ).parents( '.tab-pane' ).next().attr( 'id' );
-
-		$('[href=#' + nextId + ']' ).tab( 'show' );
-
-		return false;
-	} );
-
-	$( 'a[data-toggle="tab"]' ).on( 'shown.bs.tab', function ( event ) {
-
-		var step = $( event.target ).data( 'step' );
-		var percent = ( parseInt( step ) / 5 ) * 100;
-
-		$( '.progress-bar' ).css( {
-			width: percent + '%'
-		} );
-
-		$( '.progress-bar' ).text( 'Step ' + step + ' of 5' );
-
-	} );
-
-	$( '.first' ).on( 'click', function( event ) {
-
-		$( '#myWizard a:first' ).tab( 'show' );
-
-	} );
-
 } );
