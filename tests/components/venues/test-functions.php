1d6d0d64dc9a50ca53508973a81e435fd283afe8
DrewAPicture <info@drewapicture.com>
2018-05-27 00:27:25 -0600

    Stub out test classes for the venues component.

---
+++ tests/components/venues/test-functions.php
@@ -0,0 +1,16 @@
+<?php
+namespace Ensemble\Components\Venues;
+
+/**
+ * Venues functions tests.
+ *
+ * @since 1.0.0
+ */
+class Functions_Tests extends \WP_UnitTestCase {
+
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+}
+
---
+++ tests/components/venues/test-venues.php
@@ -0,0 +1,16 @@
+<?php
+namespace Ensemble\Components\Venues;
+
+/**
+ * General contests component tests.
+ *
+ * @since 1.0.0
+ */
+class Tests extends \WP_UnitTestCase {
+
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+}
+
