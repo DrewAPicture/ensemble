d6343ac48cc00fd0e54600191fb0fd496c8005a0
DrewAPicture <info@drewapicture.com>
2018-05-27 00:27:14 -0600

    Stub out test classes for the contests component.

---
+++ tests/components/contests/admin/test-menu.php
@@ -0,0 +1,16 @@
+<?php
+namespace Ensemble\Components\Contests\Admin;
+
+/**
+ * Contests admin menu tests.
+ *
+ * @since 1.0.0
+ */
+class Menu_Tests extends \WP_UnitTestCase {
+
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+}
+
---
+++ tests/components/contests/test-database.php
@@ -0,0 +1,16 @@
+<?php
+namespace Ensemble\Components\Contests;
+
+/**
+ * Contests database tests.
+ *
+ * @since 1.0.0
+ */
+class Database_Tests extends \WP_UnitTestCase {
+
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+}
+
