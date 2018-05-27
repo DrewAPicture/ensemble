8e62ba0bdb2a0306963de53fdff82f0f43c473fd
DrewAPicture <info@drewapicture.com>
2018-05-27 13:38:08 -0600

    Stub out test classes for first-party utilities.

---
+++ tests/util/test-registry.php
@@ -0,0 +1,18 @@
+<?php
+namespace Ensemble\Core;
+
+use Ensemble\Tests\UnitTestCase;
+
+/**
+ * Registry utility tests.
+ *
+ * @since 1.0.0
+ */
+class Registry_Tests extends UnitTestCase {
+
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+}
+
