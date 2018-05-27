ba063d4eb82d89ef681e5c84882e3a9306c03b35
DrewAPicture <info@drewapicture.com>
2018-05-27 02:49:19 -0600

    Add full test coverage for Contests\get_allowed_statuses, get_status_label, get_allowed_types, and get_type_label().

--- tests/components/contests/test-functions.php
+++ tests/components/contests/test-functions.php
@@ -28,5 +28,59 @@ class Functions_Tests extends UnitTestCase {
 		$this->assertContains( 'get_instance_invalid_id', $contest->get_error_codes() );
 	}
 
+	/**
+	 * @covers \Ensemble\Components\Contests\get_allowed_statuses
+	 */
+	public function test_get_allowed_statuses_should_return_default_statuses_and_labels() {
+		$expected = array(
+			'private'   => __( 'Private', 'ensemble' ),
+			'published' => __( 'Published', 'ensemble' ),
+		);
+
+		$this->assertEqualSetsWithIndex( $expected, get_allowed_statuses() );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Contests\get_status_label
+	 */
+	public function test_get_status_label_with_invalid_status_should_return_the_private_label() {
+		$this->assertSame( 'Private', get_status_label( 'foo' ) );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Contests\get_status_label
+	 */
+	public function test_get_status_label_with_valid_status_should_return_the_corresponding_label() {
+		$this->assertSame( 'Published', get_status_label( 'published' ) );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Contests\get_allowed_types
+	 */
+	public function test_get_allowed_types_should_return_default_types_and_labels() {
+		$expected = array(
+			'preview'  => __( 'Preview Clinic', 'ensemble' ),
+			'standard' => __( 'Standard', 'ensemble' ),
+			'special'  => __( 'Special Event', 'ensemble' ),
+			'state'    => __( 'State Championships', 'ensemble' ),
+			'camp'     => __( 'Summer Camp', 'ensemble' ),
+		);
+
+		$this->assertEqualSetsWithIndex( $expected, get_allowed_types() );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Contests\get_type_label
+	 */
+	public function test_get_type_label_with_invalid_type_should_return_the_standard_label() {
+		$this->assertSame( 'Standard', get_type_label( 'foo' ) );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Contests\get_type_label
+	 */
+	public function test_get_type_label_with_valid_type_should_return_the_corresponding_label() {
+		$this->assertSame( 'Summer Camp', get_type_label( 'camp' ) );
+	}
 }
 
