021800efd0304f1461708b4c2ecc790cb240515d
DrewAPicture <info@drewapicture.com>
2018-05-27 15:01:13 -0600

    Add six tests for Venues\get_allowed_statuses, get_status_label, get_allowed_types, and get_type_label, respectively.

--- tests/components/venues/test-functions.php
+++ tests/components/venues/test-functions.php
@@ -35,6 +35,60 @@ class Functions_Tests extends UnitTestCase {
 		$this->assertInstanceOf( '\\Ensemble\\Components\\Venues\\Model', get_venue( $venue_id ) );
 	}
 
+	/**
+	 * @covers \Ensemble\Components\Venues\get_allowed_statuses
+	 */
+	public function test_get_allowed_statuses_should_return_the_default_statuses_and_labels() {
+		$exepcted = array(
+			'active'   => __( 'Active', 'ensemble' ),
+			'inactive' => __( 'Inactive', 'ensemble' ),
+		);
+
+		$this->assertEqualSetsWithIndex( $exepcted, get_allowed_statuses() );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Venues\get_status_label
+	 */
+	public function test_get_status_label_with_invalid_status_should_return_the_inactive_status_label() {
+		$this->assertSame( 'Inactive', get_status_label( 'foo' ) );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Venues\get_status_label
+	 */
+	public function test_get_status_label_with_valid_status_should_return_that_label() {
+		$this->assertSame( 'Active', get_status_label( 'active' ) );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Venues\get_allowed_types
+	 */
+	public function test_get_allowed_types_should_return_the_default_types_and_labels() {
+		$expected = array(
+			'school' => __( 'School', 'ensemble' ),
+			'church' => __( 'Church', 'ensemble' ),
+			'center' => __( 'Community Center', 'ensemble' ),
+			'arena'  => __( 'Arena', 'ensemble' ),
+			'other'  => __( 'Other', 'ensemble' ),
+		);
+
+		$this->assertEqualSetsWithIndex( $expected, get_allowed_types() );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Venues\get_type_label
+	 */
+	public function test_get_type_label_with_invalid_type_should_return_the_school_label() {
+		$this->assertSame( 'School', get_type_label( 'foo' ) );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Venues\get_type_label
+	 */
+	public function test_get_type_label_with_valid_type_should_return_that_label() {
+		$this->assertSame( 'Community Center', get_type_label( 'center' ) );
+	}
 
 }
 
