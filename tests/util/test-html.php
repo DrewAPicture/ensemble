7e0300d65eb6cf57ad8bea917e7caa567d360aba
DrewAPicture <info@drewapicture.com>
2018-05-27 13:38:57 -0600

    Stub out a test class for the HTML utility.

---
+++ tests/util/test-html.php
@@ -0,0 +1,18 @@
+<?php
+namespace Ensemble\Core;
+
+use Ensemble\Tests\UnitTestCase;
+
+/**
+ * HTML utility tests.
+ *
+ * @since 1.0.0
+ */
+class HTML_Tests extends UnitTestCase {
+
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+}
+
