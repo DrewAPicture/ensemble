d51f27a5c19f00f996b5821beb482ebd9c928453
DrewAPicture <info@drewapicture.com>
2018-05-27 01:37:20 -0600

    Flesh out logic for creating and retrieving contest test fixtures via the factory API.

--- tests/phpunit/factories/class-ensemble-factory-for-contests.php
+++ tests/phpunit/factories/class-ensemble-factory-for-contests.php
@@ -1,6 +1,14 @@
 <?php
 namespace Ensemble\Tests\Factory;
 
+use Ensemble\Components\Contests\Database;
+use function Ensemble\Components\Contests\get_contest;
+
+/**
+ * Factory for creating, updating, and retrieving contest test fixtures.
+ *
+ * @see \WP_UnitTest_Factory_For_Thing
+ */
 class Contest extends \WP_UnitTest_Factory_For_Thing {
 
 	function __construct( $factory = null ) {
@@ -19,21 +27,38 @@ class Contest extends \WP_UnitTest_Factory_For_Thing {
 		return parent::create_and_get( $args, $generation_definitions );
 	}
 
+	/**
+	 * Creates a contest fixture.
+	 *
+	 * @param $args
+	 * @return int|\WP_Error Contest ID.
+	 */
 	function create_object( $args ) {
-		return ensemble()->contests->add( $args );
-	}
-
-	function update_object( $contest_id, $fields ) {
-		return ensemble()->contests->update( $contest_id, $fields );
+		return ( new Database )->insert( array(
+			'name'   => 'Test Contest: ' . rand_str( 6 ),
+			'venues' => array( 1, 2 ),
+		) );
 	}
 
 	/**
-	 * Stub out copy of parent method for IDE type hinting purposes.
+	 * Updates a contest fixture.
+	 *
+	 * @param $contest_id
+	 * @param $fields
+	 *
+	 * @return true|\WP_Error
+	 */
+	function update_object( $contest_id, $fields ) {
+		return ( new Database )->update( $contest_id, $fields );
+	}
+
+	/**
+	 * Retrieve a contest fixture by ID.
 	 *
 	 * @param int $contest_id Contest ID.
-	 * @return \Ensemble\Contest|false
+	 * @return \Ensemble\Components\Contests\Object|false
 	 */
 	function get_object_by_id( $contest_id ) {
-		return ensemble_get_contest( $contest_id );
+		return get_contest( $contest_id );
 	}
 }
