eeaa6c92a77456ce7ac875b3ea816d21c3f9bc0f
DrewAPicture <info@drewapicture.com>
2018-05-27 13:56:12 -0600

    Tests\Factory has to be loaded to work.

--- tests/phpunit/factory.php
+++ tests/phpunit/factory.php
@@ -1,3 +1,3 @@
 <?php
-
-require_once( dirname( __FILE__ ) . '/factories/class-ensemble-factory-for-contests.php' );
+require_once dirname( __FILE__ ) . '/factories/class-ensemble-factory-for-contests.php';
+require_once dirname( __FILE__ ) . '/factories/class-ensemble-factory.php';
