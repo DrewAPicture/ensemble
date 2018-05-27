039ff508ef52662b848ce6b1142ab4f6604a3441
DrewAPicture <info@drewapicture.com>
2018-05-27 14:06:09 -0600

    Add three tests for get_venue().

--- tests/components/venues/test-functions.php
+++ tests/components/venues/test-functions.php
@@ -10,9 +10,31 @@ use Ensemble\Tests\UnitTestCase;
  */
 class Functions_Tests extends UnitTestCase {
 
-	function test_sample() {
-		// replace this with some actual testing code
-		$this->assertTrue( true );
+	/**
+	 * @covers \Ensemble\Components\Venues\get_venue
+	 */
+	public function test_get_venue_with_invalid_id_should_return_a_WP_Error() {
+		$this->assertWPError( get_venue( null ) );
 	}
+
+	/**
+	 * @covers \Ensemble\Components\Venues\get_venue
+	 */
+	public function test_get_venue_with_invalid_id_should_return_a_WP_Error_including_code_get_instance_invalid_id() {
+		$venue = get_venue( null );
+
+		$this->assertContains( 'get_instance_invalid_id', $venue->get_error_codes() );
+	}
+
+	/**
+	 * @covers \Ensemble\Components\Venues\get_venue
+	 */
+	public function test_get_venue_with_valid_venue_id_should_return_Model() {
+		$venue_id = $this->factory->venue->create();
+
+		$this->assertInstanceOf( '\\Ensemble\\Components\\Venues\\Model', get_venue( $venue_id ) );
+	}
+
+
 }
 
