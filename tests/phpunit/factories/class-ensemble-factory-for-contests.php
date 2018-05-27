13bb188846d8c6d9346a40dbea7525305bd94c4d
DrewAPicture <info@drewapicture.com>
2018-05-27 14:05:56 -0600

    Fix some docs and types for the contests factory class.

--- tests/phpunit/factories/class-ensemble-factory-for-contests.php
+++ tests/phpunit/factories/class-ensemble-factory-for-contests.php
@@ -11,17 +11,12 @@ use function Ensemble\Components\Contests\get_contest;
  */
 class Contest extends \WP_UnitTest_Factory_For_Thing {
 
-	function __construct( $factory = null ) {
-		parent::__construct( $factory );
-	}
-
 	/**
-	 * Stub out copy of parent method for IDE type hinting purposes.
+	 * Wraps the parent method for IDE type hinting purposes.
 	 *
 	 * @param array $args
 	 * @param null  $generation_definitions
-	 *
-	 * @return \Ensemble\Contest|false
+	 * @return \Ensemble\Components\Contests\Model|\WP_Error
 	 */
 	function create_and_get( $args = array(), $generation_definitions = null ) {
 		return parent::create_and_get( $args, $generation_definitions );
@@ -31,7 +26,7 @@ class Contest extends \WP_UnitTest_Factory_For_Thing {
 	 * Creates a contest fixture.
 	 *
 	 * @param $args
-	 * @return int|\WP_Error Contest ID.
+	 * @return int|\WP_Error Contest ID or WP_Error if there was a problem.
 	 */
 	function create_object( $args ) {
 		return ( new Database )->insert( array(
@@ -56,7 +51,7 @@ class Contest extends \WP_UnitTest_Factory_For_Thing {
 	 * Retrieve a contest fixture by ID.
 	 *
 	 * @param int $contest_id Contest ID.
-	 * @return \Ensemble\Components\Contests\Object|false
+	 * @return \Ensemble\Components\Contests\Model|false
 	 */
 	function get_object_by_id( $contest_id ) {
 		return get_contest( $contest_id );
