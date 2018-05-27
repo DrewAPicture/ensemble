0f2f8a71f0c5273e1ebd18a3c0ceba0d78b18254
DrewAPicture <info@drewapicture.com>
2018-05-27 00:35:34 -0600

    Stub out a generic Ensemble tests class.

---
+++ tests/test-ensemble.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Main Ensemble bootstrap tests.
+ *
+ * @since 1.0.0
+ */
+class Ensemble_Bootstrap_Tests extends \WP_UnitTestCase {
+
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+}
+
