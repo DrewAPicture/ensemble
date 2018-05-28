e453ddb5dcfca6932f83ce63baca9feefe60da86
DrewAPicture <info@drewapicture.com>
2018-05-27 23:12:00 -0600

    Add a test each for Venues\Database->get_cache_group() and get_query_object_type().

--- tests/components/contests/test-database.php
+++ tests/components/contests/test-database.php
@@ -7,12 +7,51 @@ use Ensemble\Tests\UnitTestCase;
  * Contests database tests.
  *
  * @since 1.0.0
+ *
+ * @coversDefaultClass \Ensemble\Components\Contests\Database
+ *
+ * @group components
+ * @group contests
+ * @group database
  */
 class Database_Tests extends UnitTestCase {
 
-	function test_sample() {
-		// replace this with some actual testing code
-		$this->assertTrue( true );
+	/**
+	 * Contest database fixture.
+	 *
+	 * @var \Ensemble\Components\Contests\Database
+	 */
+	protected static $db;
+
+	/**
+	 * Contest models fixture.
+	 *
+	 * @var int[]
+	 */
+	protected static $contests;
+
+	/**
+	 * Sets up fixtures before the test class has loaded.
+	 */
+	public static function wpSetUpBeforeClass() {
+		self::$db = ( new Database );
+
+		self::$contests[] = self::ensemble()->contest->create_many( 2 );
 	}
+
+	/**
+	 * @covers ::get_cache_group()
+	 */
+	public function test_get_cache_group_should_return_contests() {
+		$this->assertSame( 'contests', self::$db->get_cache_group() );
+	}
+
+	/**
+	 * @covers ::get_query_object_type()
+	 */
+	public function test_get_query_object_type_should_return_the_fully_qualified_model_class() {
+		$this->assertSame( __NAMESPACE__ . '\\Model', self::$db->get_query_object_type() );
+	}
+
 }
 
