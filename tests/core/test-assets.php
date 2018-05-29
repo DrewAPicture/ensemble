d76055fd3b66249d87df261a552509723b3fa196
DrewAPicture <info@drewapicture.com>
2018-05-28 18:52:28 -0600

    Add two tests with data providers for registered scripts and stylesheets.

---
+++ tests/core/test-assets.php
@@ -0,0 +1,78 @@
+<?php
+namespace Ensemble\Core;
+
+use Ensemble\Tests\UnitTestCase;
+
+/**
+ * Core assets tests.
+ *
+ * @since 1.0.0
+ *
+ * @coversDefaultClass \Ensemble\Core\Assets
+ *
+ * @group assets
+ * @group core
+ */
+class Assets_Tests extends UnitTestCase {
+
+	/**
+	 * Classifications component setup fixture.
+	 *
+	 * @var \Ensemble\Core\Assets
+	 */
+	protected static $assets;
+
+	/**
+	 * Sets up fixtures before the test class has loaded.
+	 */
+	public static function wpSetUpBeforeClass() {
+		self::$assets = new Assets;
+		self::$assets->admin_assets( 'page_ensemble-admin' );
+	}
+
+
+	/**
+	 * @covers ::admin_assets()
+	 *
+	 * @dataProvider _test_admin_scripts_dp
+	 */
+	public function test_admin_scripts( $script, $script_is ) {
+		$this->assertTrue( wp_script_is( $script, $script_is ) );
+	}
+
+	/**
+	 * Data provider for test_admin_scripts().
+	 */
+	public function _test_admin_scripts_dp() {
+		return array(
+			array( 'ensbl-bootstrap', 'registered' ),
+			array( 'ensbl-parsley', 'registered' ),
+			array( 'ensbl-selectWoo', 'registered' ),
+			array( 'ensbl-admin', 'registered' ),
+		);
+	}
+
+	/**
+	 * @covers ::admin_assets()
+	 *
+	 * @dataProvider _test_admin_styles_dp
+	 */
+	public function test_admin_styles( $style, $style_is ) {
+		$this->assertTrue( wp_style_is( $style, $style_is ) );
+	}
+
+	/**
+	 * Data provider for test_admin_styles().
+	 */
+	public function _test_admin_styles_dp() {
+		return array(
+			array( 'ensbl-jquery-ui-css', 'registered' ),
+			array( 'ensbl-selectWoo-css', 'registered' ),
+			array( 'ensbl-select2-bootstrap-css', 'registered' ),
+			array( 'ensbl-bootstrap-css', 'registered' ),
+			array( 'ensbl-admin-css', 'registered' ),
+		);
+	}
+
+}
+
